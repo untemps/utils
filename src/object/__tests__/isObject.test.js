0bebdb2b1439db5d455e214437a585e055989690
untemps <v.lebadezet@untemps.net>
2022-03-19 16:18:23 +0100

    test: Fix isObject function tests

--- src/object/__tests__/isObject.test.js
+++ src/object/__tests__/isObject.test.js
@@ -4,12 +4,8 @@ describe('isObject', function () {
 	// prettier-ignore
 	it.each([
 		{ foo: 1 },
-		Object.create({ foo: 1 }),
 		Object.create(Object.prototype, { foo: { value: 1 } }),
 		Object.assign({}, { foo: 1 }),
-		Object(false),
-		Object(0),
-		Object('foo'),
 	])('returns true', (value) => {
 		expect(isObject(value)).toBeTruthy()
 	})
