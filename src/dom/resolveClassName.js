78b0c38f151cd831d71278c8b18614d8f8eed099
Vincent Le Badezet <v.lebadezet@untemps.net>
2023-02-24 23:10:48 +0100

    feat(dom): Allow to mix condition arrays and direct strings (#24)

--- src/dom/resolveClassName.js
+++ src/dom/resolveClassName.js
@@ -11,20 +11,24 @@
  * const input = [
  *  [true, 'foo', 'bar'],
  *  [false, 'foo', 'bar'],
+ *  'gag',
  * ]
- * resolveClassName(input) // 'foo bar'
+ * resolveClassName(input) // 'foo bar gag'
  *
- * @param {array<array>} input - Array of conditions to evaluate. A condition is an array with 3 items : [condition to evaluate, class name if condition is true, class name if condition is false]
+ * @param {array<array>} input - Array of conditions to evaluate. A condition is an array with 3 items : [condition to evaluate, class name if condition is true, class name if condition is false]. You may pass a classname as string instead of a condition.
  * @returns {string}    The aggregated class names.
  */
-export const resolveClassName = (input) =>
-	input
-		.reduce((acc, [condition, truthyValue, falsyValue]) => {
-			if (condition && !!truthyValue) {
-				return [...acc, truthyValue]
-			} else if (!condition && !!falsyValue) {
-				return [...acc, falsyValue]
+export const resolveClassName = (input) => {
+	if (!input?.length) return ''
+	return input
+		.reduce((acc, value) => {
+			if (Array.isArray(value)) {
+				const [condition, truthyValue, falsyValue] = value
+				value = !!condition ? truthyValue : falsyValue
 			}
-			return acc
+			return !!normalize(value) ? [...acc, value] : acc
 		}, [])
 		.join(' ')
+}
+
+const normalize = (value) => (!!value && typeof value === 'string' ? value : null)
