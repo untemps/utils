8f4999e46b7fac02e0430bcaddbd51e0f5c9009c
untemps <v.lebadezet@untemps.net>
2023-02-24 22:58:16 +0100

    feat(dom): Allow to mix condition arrays and direct strings, #23

--- src/dom/__tests__/resolveClassName.test.js
+++ src/dom/__tests__/resolveClassName.test.js
@@ -3,141 +3,139 @@ import { resolveClassName } from '../resolveClassName'
 describe('resolveClassName', () => {
 	// prettier-ignore
 	it.each([
-        {
-            input: [[]],
-            expected: ''
-        },
-        {
-            input: [[true, null]],
-            expected: ''
-        },
-        {
-            input: [[true]],
-            expected: ''
-        },
-        {
-            input: [[true, 'foo']],
-            expected: 'foo'
-        },
-        {
-            input: [[true, 'foo', 'bar']],
-            expected: 'foo'
-        },
-        {
-            input: [[false, 'foo']],
-            expected: ''
-        },
-        {
-            input: [[false, 'foo', 'bar']],
-            expected: 'bar'
-        },
-        {
-            input: [
-                [false, 'foo', 'bar'],
-                [true, 'foo', 'bar']
-            ],
-            expected: 'bar foo'
-        },
-        {
-            input: [
-                [true, 'foo', 'bar'],
-                [false, 'foo', 'bar'],
-            ],
-            expected: 'foo bar',
-        },
-        {
-            input: [
-                [false, 'foo'],
-                [true, 'foo', 'bar'],
-            ],
-            expected: 'foo',
-        },
-        {
-            input: [
-                [true, 'foo', 'bar'],
-                [false, 'foo'],
-            ],
-            expected: 'foo',
-        },
-        {
-            input: [
-                [true, 'foo', 'bar'],
-                [false, 'foo'],
-                [true, 'foo'],
-                [false, 'foo', 'bar'],
-            ],
-            expected: 'foo foo bar',
-        },
-        {
-            input: [
-                [false, 'foo', 'bar'],
-                [true, 'foo'],
-                [false, 'foo'],
-                [true, 'foo', 'bar'],
-            ],
-            expected: 'bar foo foo',
-        },
-        {
-            input: [
-                [true, 'foo'],
-                [false, 'foo', 'bar'],
-                [true, 'foo', 'bar'],
-                [false, 'foo'],
-            ],
-            expected: 'foo bar foo',
-        },
-        {
-            input: [
-                [false, 'foo'],
-                [true, 'foo', 'bar'],
-                [false, 'foo', 'bar'],
-                [true, 'foo'],
-            ],
-            expected: 'foo bar foo',
-        }
-    ])('serializes class names depending on conditions and provided values', ({input, expected}) => {
-        expect(resolveClassName(input)).toBe(expected)
-    })
-
-	// prettier-ignore
-	it.each([
-		{
-			name: 'throws if input is null',
-			input: null,
-		},
-		{
-			name: 'throws if input is undefined',
-			input: undefined,
-		},
-		{
-			name: 'throws if input is not an array',
-			input: {},
-		},
-		{
-			name: 'throws if input is not stringifiable',
-			input: Symbol(),
-		},
-		{
-			name: 'throws if input contains null values',
-			input: [null],
-		},
-		{
-			name: 'throws if input contains undefined values',
-			input: [undefined],
-		},
-		{
-			name: 'throws if input does contain arrays',
-			input: [{}],
-		},
-		{
-			name: 'throws if input does contain stringifiable values',
-			input: [Symbol()],
-		},
-		{
-			name: 'throws if input does contain stringifiable values',
-			input: [true, 'foo'],
-		},
-	])('$name', ({ input }) => {
-		expect(() => resolveClassName(input)).toThrow()
-	})
+    {
+      input: null,
+      expected: ''
+    },
+    {
+      input: undefined,
+      expected: ''
+    },
+    {
+      input: '',
+      expected: ''
+    },
+    {
+      input: [],
+      expected: ''
+    },
+    {
+      input: {},
+      expected: ''
+    },
+    {
+      input: Symbol(),
+      expected: ''
+    },
+    {
+      input: [[true, null]],
+      expected: ''
+    },
+    {
+      input: [[true]],
+      expected: ''
+    },
+    {
+      input: [[true, 'foo']],
+      expected: 'foo'
+    },
+    {
+      input: [[true, 'foo', 'bar']],
+      expected: 'foo'
+    },
+    {
+      input: [[false, 'foo']],
+      expected: ''
+    },
+    {
+      input: [[false, 'foo', 'bar']],
+      expected: 'bar'
+    },
+    {
+      input: [
+        [false, 'foo', 'bar'],
+        [true, 'foo', 'bar']
+      ],
+      expected: 'bar foo'
+    },
+    {
+      input: [
+        [true, 'foo', 'bar'],
+        [false, 'foo', 'bar'],
+      ],
+      expected: 'foo bar',
+    },
+    {
+      input: [
+        [false, 'foo'],
+        [true, 'foo', 'bar'],
+      ],
+      expected: 'foo',
+    },
+    {
+      input: [
+        [true, 'foo', 'bar'],
+        [false, 'foo'],
+      ],
+      expected: 'foo',
+    },
+    {
+      input: [
+        [true, 'foo', 'bar'],
+        [false, 'foo'],
+        [true, 'foo'],
+        [false, 'foo', 'bar'],
+      ],
+      expected: 'foo foo bar',
+    },
+    {
+      input: [
+        [false, 'foo', 'bar'],
+        [true, 'foo'],
+        [false, 'foo'],
+        [true, 'foo', 'bar'],
+      ],
+      expected: 'bar foo foo',
+    },
+    {
+      input: [
+        [true, 'foo'],
+        [false, 'foo', 'bar'],
+        [true, 'foo', 'bar'],
+        [false, 'foo'],
+      ],
+      expected: 'foo bar foo',
+    },
+    {
+      input: [
+        [false, 'foo'],
+        [true, 'foo', 'bar'],
+        [false, 'foo', 'bar'],
+        [true, 'foo'],
+      ],
+      expected: 'foo bar foo',
+    },
+    {
+      input: [
+        [true, "footrue", "foofalse"],
+        [false, "poltrue", "polfalse"],
+        "bar",
+        [false, "gagtrue"],
+        99,
+        [true, 99],
+        [true, "samtrue"],
+        [false, "ziztrue", "zizfalse"]
+      ],
+      expected: 'footrue polfalse bar samtrue zizfalse',
+    },
+    {
+      input: [
+        'bar',
+        [false, 'foo'],
+      ],
+      expected: 'bar',
+    }
+  ])('serializes class names depending on conditions and provided values', ({input, expected}) => {
+    expect(resolveClassName(input)).toBe(expected)
+  })
 })
