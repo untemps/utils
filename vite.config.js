3bd4e91c384dea821cd0a1bf384b50cb148da370
untemps <v.lebadezet@untemps.net>
2026-03-27 19:06:08 +0100

    chore: Remove glob devDependency

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import glob from 'glob';
+import { globSync } from 'node:fs';
 import { extname, relative } from 'node:path'
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite'
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       name: '@untemps/utils',
       entry: Object.fromEntries(
-        glob.sync('src/**/*.js').filter(file => !file.includes('__tests__')).map(file => [
+        globSync('src/**/*.js', { exclude: (file) => file.includes('__tests__') }).map(file => [
             relative("src", file.slice(0, file.length - extname(file).length)),
             fileURLToPath(new URL(file, import.meta.url))
           ]
