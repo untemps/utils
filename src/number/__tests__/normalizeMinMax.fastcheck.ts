734b947dcd81818e7aab64a4f2677bcf79e6f32b
Vincent Le Badezet <v.lebadezet@untemps.net>
2026-05-29 19:27:51 +0200

    test(number): Add description to normalizeMinMax property test (#84)
    (cherry picked from commit b5434c616c773fcbc8aa2c1857b7bcae6952fa78)

--- src/number/__tests__/normalizeMinMax.fastcheck.ts
+++ src/number/__tests__/normalizeMinMax.fastcheck.ts
@@ -3,7 +3,7 @@ import * as fc from 'fast-check'
 import { normalizeMinMax } from '../normalizeMinMax'
 
 describe('normalizeMinMax', () => {
-	it('', () => {
+	it('always returns min less than or equal to max', () => {
 		fc.assert(
 			fc.property(fc.integer({ min: -10, max: 100 }), fc.integer({ min: -10, max: 100 }), (pMin, pMax) => {
 				const { min, max } = normalizeMinMax(pMin, pMax)
